1a7ba4b5fef1c1299e8cb651a68f055e1bd44b71
des-ebiz <des@ebiznetworks.co.kr>
2020-05-21 11:58:52 +0900

    edit

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 function PostList({ posts }) {
   return (
     <ul>
+      <br />
       {posts.map((item, i) => (
         <li key={i}>{item.title}</li>
       ))}
