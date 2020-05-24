f57a8a4bdee3a675425454569774ebeb4760fea8
vxggangxv <vxggangxv@naver.com>
2020-05-24 09:46:46 +0900

    edit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,20 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import { lazy } from '@loadable/component';
 // import Spinner from '../components/common/Spinner';
 
-const HomePage = lazy(() => import('../pages/HomePage'));
-// const PostListPage = lazy(() => import('../pages/PostListPage'));
-// const PostPage = lazy(() => import('../pages/PostPage'));
-// const CounterPage = lazy(() => import('../pages/CounterPage'));
+// const HomePage = lazy(() => import('../pages/HomePage'));
+const PostListPage = lazy(() => import('../pages/PostListPage'));
+const PostPage = lazy(() => import('../pages/PostPage'));
+const CounterPage = lazy(() => import('../pages/CounterPage'));
 
 export default () => (
   <>
     <Suspense fallback={<div></div>}>
       <Switch>
-        <Redirect path="/" exact to="/home" />
-        <Route path="/home" component={HomePage} />
-        {/* <Route path="/posts" exact component={PostListPage} />
+        <Redirect path="/" exact to="/posts" />
+        {/* <Route path="/home" component={HomePage} /> */}
+        <Route path="/posts" exact component={PostListPage} />
         <Route path="/posts/:id" component={PostPage} />
-        <Route path="/counter" component={CounterPage} /> */}
+        <Route path="/counter" component={CounterPage} />
         <Redirect from="*" to="/" />
       </Switch>
     </Suspense>
